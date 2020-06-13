8e8f18623bb782a49fc4427517bbdc85e40ae52f
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-06-13 18:02:56 -0500

    Add files via upload

---
+++ mstech_estarbien_ecommerce/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Proyecto - Ventas MSTECH',
+    'description': "Integración pryecto - ventas para el ecommerce de Estarbien",
+    'author': "MSTECH",
+    'website': "https://www.mstech.pe",
+    'summary': "Integración pryecto - ventas para el ecommerce de Estarbien",
+    'version': '13.0.1.0.0',
+    'depends': [
+        'sale_timesheet'
+    ],
+    'installable': True,
+    'sequence': 1,
+}
---
+++ mstech_estarbien_ecommerce/models/sale_order.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError, Warning
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+    
+    def _timesheet_service_generation(self):
+        """ For service lines, create the task or the project. If already exists, it simply links
+            the existing one to the line.
+            Note: If the SO was confirmed, cancelled, set to draft then confirmed, avoid creating a
+            new project/task. This explains the searches on 'sale_line_id' on project/task. This also
+            implied if so line of generated task has been modified, we may regenerate it.
+        """
+        super(SaleOrderLine, self)._timesheet_service_generation()
+        so_line_estarbien = self.filtered(lambda sol: sol.is_service and sol.product_id.service_tracking == 'no')
+        
+        usuario = self.env.user
+        project = self.env['project.project'].sudo().search([('user_id','=',usuario.id)], order='create_date desc' limit=1)
+        for so_line in so_line_estarbien :
+            if not project :
+                if not so_line.project_id :
+                    project = so_line._timesheet_create_project()
+                    project.sudo().write({'user_id': usuario.id, 'sale_line_id': False, 'sale_order_id': False, 'name': so_line.order_id.partner_id.display_name})
+                else :
+                    project = so_line.project_id
+            elif (not so_line.project_id) or so_line.project_id != project :
+                so_line.project_id = project
+            if not so_line.task_id :
+                so_line._timesheet_create_task(project=project)
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        if isinstance(vals_list, list) :
+            copia_list = vals_list[:]
+            i = 0
+            for vals in vals_list :
+                i = i + 1
+                if vals.get('product_uom_qty', 0) > 1 :
+                    producto = self.env['product.product'].browse(vals['product_id'])
+                    if producto.type == 'service' and producto.service_tracking == 'no' :
+                        copia_list[i]['product_uom_qty'] = 1
+                        copia_list.extend([copia_list[i]]*(vals.get('product_uom_qty', 0)-1))
+            vals_list = copia_list[:]
+        else :
+            copia = dict(vals_list)
+            if vals_list.get('product_uom_qty', 0) > 1 :
+                producto = self.env['product.product'].browse(vals_list['product_id'])
+                if producto.type == 'service' and producto.service_tracking == 'no' :
+                    copia[i]['product_uom_qty'] = 1
+                    copia.extend([copia[i]]*(vals_list.get('product_uom_qty', 0)-1))
+            vals_list = dict(copia)
+        return super(SaleOrderLine, self).create(vals_list)
+    #################################################
