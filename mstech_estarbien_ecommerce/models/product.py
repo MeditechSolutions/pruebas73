58c7be0a1d70fad271ed83da3fdd5597ad1ea5e6
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-06-16 17:50:50 -0500

    Add files via upload

--- mstech_estarbien_ecommerce/models/product.py
+++ mstech_estarbien_ecommerce/models/product.py
@@ -3,10 +3,19 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError, Warning
 
+class ProductTemplateProtocolo(models.Model) :
+    _name = 'product.template.protocolo'
+    _description = 'Protocolo de atención'
+    
+    name = fields.Char(string='Protocolo')
+    estacion_ids = fields.Many2many(comodel_name='project.task.type', string='Estaciones', domain=[('estacion_estarbien','=',True)])
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
     service_tracking = fields.Selection(selection_add=[('estarbien', 'Estarbien')])
+    subproducto_ids = fields.Many2many(comodel_name='product.template', relation='tabla_bom_extra', column1='prod1', column2='prod2', string='Subproductos')
+    protocolo_id = fields.Many2one(comodel_name='product.template.protocolo',string='Perfil')
     
     @api.constrains('project_id', 'project_template_id')
     def _check_project_and_template(self) :
