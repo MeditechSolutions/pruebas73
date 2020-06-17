9bb80f58275aa6f23a8f95b550284db6f904cc69
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-06-17 12:46:52 -0500

    Add files via upload

--- mstech_estarbien_ecommerce/models/project.py
+++ mstech_estarbien_ecommerce/models/project.py
@@ -3,12 +3,18 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError, Warning
 
+class ProjectTaskType(models.Model) :
+    _inherit = 'project.task.type'
+    
+    estacion_estarbien = fields.Boolean(string='Estación')
+    codigo_estarbien = fields.Char(string='Código Estarbien')
+
 class ProjectTask(models.Model) :
     _inherit = 'project.task'
     
     paciente_id = fields.Many2one(comodel_name='res.partner', string='Paciente', track_visibility='onchange')
     cita_id = fields.Many2one(comodel_name='medicina.cita', string='Cita', track_visibility='onchange')
-    estacion_ids = fields.Many2many(related='sale_line_id.product_id.protocolo_id.estacion_ids', string='Estaciones', store=True, readonly=False, domain=[('estacion_estarbien','=',True)])
+    estacion_ids = fields.Many2many(comodel_name='project.task.type', related='sale_line_id.product_id.protocolo_id.estacion_ids', string='Estaciones', store=True, readonly=False, domain=[('estacion_estarbien','=',True)])
     tipo_cita = fields.Char(related='stage_id.codigo_estarbien', string='Tipo de cita')
     
     def write(self, values) :
@@ -28,15 +34,9 @@ class ProjectTask(models.Model) :
                     valor = self.sale_line_id._timesheet_create_task_prepare_values(self.project_id)
                     valor.update({'parent_id': self.id})
                     del valor['sale_line_id']
-                    for elemento in self.sale_line_id.product_id.protocolo_id.estacion_ids :
+                    for elemento in self.estacion_ids :
                         especial = dict(valor)
                         especial.update({'stage_id': elemento.id})
                         task = self.env['project.task'].sudo().create(especial)
         res = super(ProjectTask, self).sudo().write(values)
         return res
-
-class ProjectTaskType(models.Model) :
-    _inherit = 'project.task.type'
-    
-    estacion_estarbien = fields.Boolean(string='Estación')
-    codigo_estarbien = fields.Char(string='Código Estarbien')
