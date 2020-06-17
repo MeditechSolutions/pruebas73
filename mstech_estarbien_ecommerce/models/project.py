3833434cc61a488cac0e8e87f45f2092880af350
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-06-16 19:21:31 -0500

    Update project.py

--- mstech_estarbien_ecommerce/models/project.py
+++ mstech_estarbien_ecommerce/models/project.py
@@ -21,6 +21,14 @@ class ProjectTask(models.Model) :
                     valor.append((0, 0, {'res_model': 'project.task', 'res_id': self.id, 'partner_id': self.env['medical.patient'].browse(values.get('paciente_id')).patient_id.id}))
                 if valor :
                     values.update({'message_follower_ids': valor})
+                if self.subtask_count == 0 :
+                    valor = self.sale_line_id._timesheet_create_task_prepare_values(self.project_id)
+                    valor.update({'parent_id': self.id})
+                    del valor['sale_line_id']
+                    for elemento in self.sale_line_id.product_id.protocolo_id.estacion_ids :
+                        especial = dict(valor)
+                        especial.update({'stage_id': elemento.id})
+                        task = self.env['project.task'].sudo().create(especial)
         res = super(ProjectTask, self).write(values)
         return res
 
